a2a7ffd3878ac94deefabd76226fb451674aa23a
Theodore Winberg <tedwinberg@Theodores-MacBook-Pro.local>
2018-09-01 22:32:20 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Helloooo world!</h1>
+    <p>My name is Theodore Winberg and I like coding and winning competitions!</p>
+    <p>Also slacking off is fun. But THAT'S NOT WHY I'M HERE!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
